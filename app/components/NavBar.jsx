aa8bf99a7c2258251450749a355e5a524f64a88c
Raj <rajsunmukhani123@gmail.com>
2024-07-14 17:31:39 +0530

    first class

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='text-2xl py-5 text-white bg-zinc-400 flex items-center justify-center'>
+        <h1>NavBar</h1>
+    </div>
+  )
+}
+
+export default NavBar
